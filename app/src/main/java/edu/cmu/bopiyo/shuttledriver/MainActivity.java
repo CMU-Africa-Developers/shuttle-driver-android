f48fa3129531eb7c9aafc0971967220ace07ca96
Bryan Lamtoo <bryanlamtoo@gmail.com>
2019-09-05 20:10:44 +0200

    Created Repo

---
+++ app/src/main/java/edu/cmu/bopiyo/shuttledriver/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cmu.bopiyo.shuttledriver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
